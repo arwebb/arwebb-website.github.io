b3b55d1810284b86036c6c4f650278844718cbef
Annie Rose Webb <31518566+arwebb@users.noreply.github.com>
2019-04-16 14:41:50 -0400

    Update and rename main.js to game.js

--- js/game.js
+++ js/game.js
@@ -7,24 +7,24 @@ $(document).ready(function () {
   $("#proceed").hide().delay(8300).fadeIn(800);
   $("#inputLine").hide().delay(8500).fadeIn(800);
   $("#help").hide().delay(8500).fadeIn(800);
-});
+}); 
 
 /* Input box and response options */
 function enter(e) {
   if(event.key === "Enter" || e.keyCode === 13){
     var valueEntered = e.value.toUpperCase();
-    
+
     /* MOVEMENT START */
     /* EXIT */
-    if (valueEntered === "EXIT" || valueEntered === "SOUTH" || valueEntered === "GO SOUTH"){
+    if (valueEntered === "EXIT" || valueEntered === "SOUTH" || valueEntered === "GO SOUTH" || valueEntered === "QUIT"){
       $("#contact-buttons").fadeOut(200);
       $("#resume").fadeOut(200);
       $("#response").fadeOut(200);
       setTimeout(function(){$("#response").text("You feel a gentle breeze on your face as you turn SOUTH towards the exit...").fadeIn(400);}, 250);
-      setTimeout(function(){window.location.href = "simple.html";}, 2700);
+      setTimeout(function(){window.location.href = "index.html";}, 2700);
     }
     /* LIBRARY */
-    else if (valueEntered === "NORTH" || valueEntered === "GO NORTH"){
+    else if (valueEntered === "NORTH" || valueEntered === "GO NORTH" || valueEntered === "GO TO LIBRARY" || valueEntered === "GO LIBRARY"){
       $("#contact-buttons").fadeOut(200);
       $("#resume").fadeOut(200);
       $("#response").fadeOut(200);
@@ -47,10 +47,10 @@ function enter(e) {
       $("#contact-buttons").delay(1600).fadeIn(400);
     }
     /* MOVEMENT END */
-    
+
     /* INVESTIGATION START */
     /* NAME OR BANNER */
-    else if (valueEntered === "ANNIE ROSE WEBB" || valueEntered === "LOOK ANNIE ROSE WEBB" || valueEntered === "BANNER" || valueEntered === "LOOK BANNER" || valueEntered === "LOOK AT BANNER" || valueEntered === "LOOK UP"){
+    else if (valueEntered === "ANNIE ROSE WEBB" || valueEntered === "LOOK ANNIE ROSE WEBB" || valueEntered === "BANNER" || valueEntered === "LOOK BANNER" || valueEntered === "LOOK AT BANNER" || valueEntered === "LOOK AT ANNIE ROSE WEBB" || valueEntered === "LOOK AROUND"){
       $("#contact-buttons").fadeOut(200);
       $("#resume").fadeOut(200);
       $("#response").fadeOut(200);
@@ -85,7 +85,7 @@ function enter(e) {
       setTimeout(function(){$("#response").text("The exit leads out to a simple, open room. It feels somehow less... interactive than this one.").fadeIn(400);}, 250);
     }
     /* INVESTIGATION END */
-    
+
     /* GENERAL OPTIONS START */
     else if (valueEntered === "OPTIONS"){
       $("#response").fadeOut(200);
@@ -93,7 +93,7 @@ function enter(e) {
     }
     else if (valueEntered === "HELP"){
       $("#response").fadeOut(200);
-      setTimeout(function(){$("#response").text("This is a personal website that imitates a text-based game. From here, you can type LOOK to investigate any object in the room, type a direction to navigate, type EXIT to go to my non-interactable website, or close this tab and never think about it again.").fadeIn(400);}, 250);
+      setTimeout(function(){$("#response").text("This is a personal website that imitates a text-based game. Type next to the arrow above and press ENTER to interact and explore. Try typing something like LOOK BUTTONS to look around; GO WEST to navigate; or EXIT to go to my non-interactable resume. (Or close this tab and never think about it again.)").fadeIn(400);}, 250);
     }
     else if (valueEntered === "GET YE FLASK" || valueEntered === "GET FLASK" || valueEntered === "TAKE FLASK"){
       $("#response").fadeOut(200);
